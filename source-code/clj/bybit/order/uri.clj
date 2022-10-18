036027c72db2f6a603c47c1ecee606e2640a89a7
Bit Handshake <adam@monotech.hu>
2022-10-18 13:48:21 +0200

    b0.2.6 #1 - initial commit

---
+++ source-code/clj/bybit/order/uri.clj
@@ -0,0 +1,31 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns bybit.order.uri
+    (:require [bybit.uri.config :as uri.config]))
+
+
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(defn order-create-uri
+  ; @param (map) uri-props
+  ;  {:use-mainnet? (boolean)(opt)
+  ;    Default: false}
+  ;
+  ; @example
+  ;  (bybit/order-create-uri {})
+  ;  =>
+  ;  https://api-testnet.bybit.com/v2/private/order/create
+  ;
+  ; @example
+  ;  (bybit/order-create-uri {:use-mainnet? true})
+  ;  =>
+  ;  https://api.bybit.com/v2/private/order/create
+  ;
+  ; @return (string)
+  [{:keys [use-mainnet?]}]
+  (let [address (if use-mainnet? uri.config/PRIVATE-API-ADDRESS uri.config/PRIVATE-TEST-API-ADDRESS)]
+       (str address "/order/create")))
---
+++ source-code/clj/bybit/response/errors.clj
@@ -0,0 +1,35 @@
+
+;; -- Namespace ---------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(ns bybit.response.errors)
+
+
+
+;; ----------------------------------------------------------------------------
+;; ----------------------------------------------------------------------------
+
+(defn response-body->error?
+  ; @param (map) response-body
+  ;  {:ret-code (integer)
+  ;   :ret-msg (string)}
+  ;
+  ; @usage
+  ;  (bybit/response-body->error? {...})
+  ;
+  ; @return (boolean)
+  [{:keys [ret-code ret-msg]}]
+  (or (not= ret-code 0)
+      (not= ret-msg "OK")))
+
+(defn response-body->invalid-api-details?
+  ; @param (map) response-body
+  ;  {:ret-code (integer)}
+  ;
+  ; @usage
+  ;  (bybit/response-body->invalid-api-details? {:ret-code 0})
+  ;
+  ; @return (boolean)
+  [{:keys [ret-code]}]
+  (or (= ret-code 10003)
+      (= ret-code 10004)))
